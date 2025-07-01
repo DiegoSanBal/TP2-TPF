c21a8038e5c87448116e2b1324dc7b032b746c9a
DiegoSanBal <dsanturtun@Diegos-MacBook-Pro.local>
2025-06-30 22:18:48 -0300

    Profesors CRUD listo

--- services/ProfesorServices.js
+++ services/ProfesorServices.js
@@ -1,40 +1,39 @@
 import { Profesor } from "../models/index.js";
 
 class ProfesorServices {
-    getAllProfesorsServices = async () => {
-        const profesors = await Profesor.findAll();
-        return profesors;
-    };
-    getProfesorServiceById = (id) => {
-        const profesor = User.findByPk(id);
-        if (!profesor) {
-            throw new Error("Profesor not found")
-        }
-        return profesor;
-    };
-
-    createProfesorServices = async (data) => {
-        const { id, name } = await Profesor.create(data);
-        return { id, name };
-    };
-
-    updateProfesorServices = async (id, name) => {
-        const profesor = await Profesor.findByPk(id);
-        if (!profesor) throw new Error("Profesor not found");
-        profesor.name = name;
-        await profesor.save();
-
-        return { id: profesor.id, name: profesor.name }
+  getAllProfesorsServices = async () => {
+    const profesors = await Profesor.findAll();
+    return profesors;
+  };
+  getProfesorServicesById = (id) => {
+    const profesor = Profesor.findByPk(id);
+    if (!profesor) {
+      throw new Error("Profesor not found");
     }
+    return profesor;
+  };
 
-    deleteProfesorServices = async (id) => {
-        const profesor = await Profesor.findByPk(id);
-        if (!profesor) {
-            throw new Error("Profesor not found")
-        }
-        await profesor.destroy()
-        return { message: `Profesor ${id} deleted.` }
+  createProfesorServices = async (data) => {
+    const { id, name } = await Profesor.create(data);
+    return { id, name };
+  };
+
+  updateProfesorServices = async (id, data) => {
+    const profesor = await Profesor.findByPk(id);
+    if (!profesor) throw new Error("Profesor not found");
+    await profesor.update(data);
+
+    return profesor;
+  };
+
+  deleteProfesorServices = async (id) => {
+    const profesor = await Profesor.findByPk(id);
+    if (!profesor) {
+      throw new Error("Profesor not found");
     }
+    await profesor.destroy();
+    return { message: `Profesor ${id} deleted.` };
+  };
 }
 
-export default ProfesorServices; r
+export default ProfesorServices;
